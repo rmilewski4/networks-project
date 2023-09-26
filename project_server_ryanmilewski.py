4315d3218e8937034e2cc3e62f0b771501126c5a
Ryan Milewski <rsmbby@umsystem.edu>
2023-09-26 11:15:12 -0500

    Fix sockets

--- project_server_ryanmilewski.py
+++ project_server_ryanmilewski.py
@@ -8,26 +8,30 @@ import socket
 
 ipaddr = "127.0.0.1"
 port = 17022
-
+#implement checking if file exists
 def setupAccount(username, password):
     print(username + password)
-    file = open("users.txt","w")
+    file = open("users.txt","a")
 
 
 def loop():
     serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     serversocket.bind((ipaddr, port))
     serversocket.listen(5)
-    while True:
-        print("waiting for connections")
-        (clientsocket, address) = serversocket.accept()
-        dataRecv = serversocket.recv(1024)
-        #dataArr = dataRecv.split(' ')
-        #if dataArr[0] == "login":
-        #    print()
-        #elif dataArr[0] == "newuser":
-        #    setupAccount(dataArr[1], dataArr[2])
-        #serversocket.close()
+    print("waiting for connections")
+    (clientsocket, address) = serversocket.accept()
+    with clientsocket:
+        print(f"Connected by {address}")
+        while True:
+            dataRecv = clientsocket.recv(1024)
+            decodedData = dataRecv.decode()
+            print(decodedData)
+            dataArr = decodedData.split(' ')
+            if dataArr[0] == "login":
+                print()
+            elif dataArr[0] == "newuser":
+                setupAccount(dataArr[1], dataArr[2])
+            #clientsocket.close()
 
 
 if __name__ == "__main__":
